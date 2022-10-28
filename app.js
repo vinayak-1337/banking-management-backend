fe237c31a9a37c13c7ef11db88d141ed69c05d38
vinayak <vinayak@gkmit.co>
2022-10-28 11:21:43 +0530

    add port variable

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
-app.listen(process.env.DB_PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("---------<start>----------");
   connection.getConnection((err, connection) => {
     if (err) throw err;
